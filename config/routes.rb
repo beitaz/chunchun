59ff297c5826c5d55feb2660d3f441f668998738
beitaz <beitaz@icloud.com>
2018-05-22 13:21:11 +0800

    添加 grape 和 grape-swagger

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get 'static/contact'
   get 'static/about'
 
-  # mount RootAPI => '/'
-  # mount GrapeSwaggerRails::Engine => '/swagger'
+  mount RootAPI => '/'
+  mount GrapeSwaggerRails::Engine => '/swagger'
   root 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
